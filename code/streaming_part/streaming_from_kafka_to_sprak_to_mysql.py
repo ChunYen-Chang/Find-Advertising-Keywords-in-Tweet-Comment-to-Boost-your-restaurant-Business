4c05e489fc50a6fff410f10c4c6aeb91d3afb426
ChunYen Chang <fikrusnk@gmail.com>
2019-10-12 00:11:04 +0800

    DOCS: Put code comments

--- code/streaming_part/streaming_from_kafka_to_sprak_to_mysql.py
+++ code/streaming_part/streaming_from_kafka_to_sprak_to_mysql.py
@@ -6,9 +6,17 @@ from textblob import TextBlob
 
 
 # define a function
-def convert_to_dataframe(rdd):
+def convert_to_dataframe_and_save_to_mysql(rdd):
+	'''
+	Description: This function helps users to convert RDD to dataframe and save the dataframe 
+				 in a MySql database
+				
+	Parameters: rdd
+	
+	Returns: rdd
+	'''
     if not rdd.isEmpty():
-        df = spark.createDataFrame(rdd, ['text', 'polarity', 'subjectivity'])
+        df = spark.createDataFrame(rdd, ['id', 'receivetime', 'text', 'polarity', 'subjectivity'])
         df.write.format('jdbc').options(url="jdbc:mysql://<your mysql db IP>:<your mysql db port>/<db name>",driver="com.mysql.jdbc.Driver",dbtable="<db table name>",user="<db account>",password="<db password>").mode('append').save()
     return rdd
 
@@ -18,7 +26,7 @@ spark = SparkSession \
 .builder \
 .config("spark.jars.packages", "org.apache.spark:spark-streaming-kafka-0-8_2.11:2.2.0") \
 .getOrCreate()
-ssc = StreamingContext(spark.sparkContext, 10)
+ssc = StreamingContext(spark.sparkContext, 30)
 
 
 # receive data from Kafka cluster
@@ -27,8 +35,9 @@ directKafkaStream = KafkaUtils.createDirectStream(ssc, ['restaurant'], {"metadat
 
 # Data wrangling process by using RDD
 lines = directKafkaStream.map(lambda x: x[1])
-sentiment_analysis = lines.map(lambda x: (x, TextBlob(x)))
-sentiment_analysis_result = sentiment_analysis.map(lambda x: (x[0].encode('utf-8'), x[1].sentiment.polarity, x[1].subjectivity))
+lines_split = lines.map(lambda x: (x.split('|||||')[0], x.split('|||||')[1], x.split('|||||')[2]))
+sentiment_analysis = lines_split.map(lambda x: (x[0], x[2], x[1], TextBlob(x[1])))
+sentiment_analysis_result = sentiment_analysis.map(lambda x: (x[0].encode('utf-8'), x[1].encode('utf-8'), x[2].encode('utf-8'), x[3].sentiment.polarity, x[3].subjectivity))
 sentiment_analysis_result.foreachRDD(lambda x: convert_to_dataframe(x))
 
 
